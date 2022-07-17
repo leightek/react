eae0b0804ec9ad3bc00ef58d69540300ad5fb9de
leightek <leighsuconsulting@gmail.com>
2022-07-17 01:28:22 -0700

    [React2022] Logged JSX content

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-ReactDOM.render(<h1 className="header">Hello, React</h1>, document.getElementById("root"))
+const h1 = document.createElement("h1")
+h1.textContent = "Hello World"
+h1.className = "header"
+console.log(h1)
 
-// const h1 = document.createElement("h1")
-// h1.textContent = "This is an imperative way to program"
-// h1.className = "header"
-// document.getElementById("root").append(h1)
+// <h1 class="header">Hello World</h1
+
+const element = <h1 className="header">This is JSX</h1>
+console.log(element)
+
+/*
+{
+    type: "h1"
+    key: null
+    ref: null
+    props: {
+        className: "header",
+        children: "This is JSX"
+    },
+    _owner: null,
+    _store: {}
+}
+ */
+
+// JSX
+ReactDOM.render(element, document.getElementById("root"))
