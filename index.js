7d8ef7450a97f90aabb0abad6034abfb88abb40c
leightek <leighsuconsulting@gmail.com>
2022-07-17 01:43:13 -0700

    [React2022] Added parentheses and logging to the variable

--- index.js
+++ index.js
@@ -23,10 +23,15 @@ console.log(element)
  */
 
 // JSX
-const page = <div>
+const page = (
+    <div>
         <h1 className="header">This is JSX</h1>
         <p>This is a paragraph</p>,
     </div>
+)
+
+console.log(page)
+
 ReactDOM.render(
     page,
     document.getElementById("root")
