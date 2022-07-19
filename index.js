41e9c182d73e9d9d13131274afee326c0905ab76
leightek <leighsuconsulting@gmail.com>
2022-07-18 22:00:59 -0700

    [React2022] Added solution for Challenge 1

--- index.js
+++ index.js
@@ -23,16 +23,20 @@ console.log(element)
  */
 
 // JSX
-const page = (
-    <div>
-        <h1 className="header">This is JSX</h1>
-        <p>This is a paragraph</p>,
-    </div>
+const navbar = (
+    <nav>
+        <h1>Uncle Tom's Cottage</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
 )
 
-console.log(page)
+console.log(navbar)
 
 ReactDOM.render(
-    page,
+    navbar,
     document.getElementById("root")
 )
